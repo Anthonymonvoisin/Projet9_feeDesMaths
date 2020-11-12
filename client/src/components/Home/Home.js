97c816ab57e7e6d5bd5c2e1caa7387ce63e5c0ef
Thibault-Goedert <thibault.goedert@student.yncrea.com>
2020-11-12 17:04:08 +0100

    PageHome ThibaultG
    J'ai ajouté les photos du caroussel dans la page home ainsi que des textes en rapport avec notre site.

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,6 +3,9 @@ import './Home.css';
 import cerf from '../../Images/cerf.jpg'
 import loup from '../../Images/loup.jpg'
 import panthere from '../../Images/panthere.jpg'
+import mathe from '../../Images/math.jpg'
+import hommeordi from '../../Images/hommeordi.jpg'
+import ecriture from '../../Images/ecriture.jpg'
 
 /*import { } from '../Components/Components'; // liste des components a import*/
 
@@ -32,13 +35,13 @@ export const Carrousel = () =>{
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img class="d-block w-100" src={loup} height="300px" alt="First slide"></img>
+                            <img class="d-block w-100" src={mathe} height="300px" alt="First slide"></img>
                         </div>
                         <div class="carousel-item">
-                            <img class="d-block w-100" src={panthere} height="300px" alt="Second slide"></img>
+                            <img class="d-block w-100" src={ecriture} height="300px" alt="Second slide"></img>
                         </div>
                         <div class="carousel-item">
-                            <img class="d-block w-100" src={cerf} height="300px" alt="Third slide"/>
+                            <img class="d-block w-100" src={hommeordi} height="300px" alt="Third slide"/>
                         </div>
                     </div>
                 </div>
@@ -61,13 +64,13 @@ export const Txt = () =>{
     return(
         <div class="container" id="grdcontainer">
         <div class="row" id="contenue">
-            <div class="col-12" id="ligne" ><p>Taciti justo curabitur augue, sociosqu hendrerit. Bibendum condimentum est a cursus volutpat habitant. Interdum eu nisl sem euismod tempor ipsum aliquet, vel inceptos imperdiet. Tortor eros quisque netus neque sociis erat aptent dictum lorem. A, scelerisque blandit facilisis augue congue primis ante dolor inceptos scelerisque consectetur mauris? Justo orci vestibulum ligula gravida! Fermentum magna facilisi sodales duis vitae a tempor penatibus. Hendrerit penatibus suspendisse consectetur mus fames litora nulla purus. Potenti auctor natoque nostra pretium lorem imperdiet. Integer curae;.</p></div>
-            <div class="col-5">Taciti justo curabitur augue, sociosqu hendrerit. Bibendum condimentum est a cursus volutpat habitant. Interdum eu nisl sem euismod tempor ipsum aliquet, vel inceptos imperdiet. Tortor eros quisque netus neque sociis erat aptent dictum lorem. A, scelerisque blandit facilisis augue congue primis ante dolor inceptos scelerisque consectetur mauris? Justo orci vestibulum ligula gravida! Fermentum magna facilisi sodales duis vitae a tempor penatibus. Hendrerit penatibus suspendisse consectetur mus fames litora nulla purus. Potenti auctor natoque nostra pretium lorem imperdiet. Integer curae;.</div>
+            <div class="col-12" id="ligne" ><p>Bienvenue sur Fée des maths ! Sur ce site vous trouverez des cours sous format vidéo avec de nombreux quizz d’applications ainsi que des exercices avec corrections. Bienvenue dans le monde merveilleux des mathématiques ! Bonne visite et bons progrès !</p></div>
+            <div class="col-5">Non ! Vous n’êtes pas nul en maths ! Si vos résultats vous font penser le contraire, beaucoup de professeurs l’affirment : personne n’est vraiment « nul » en mathématiques. Si vous pensez toujours le contraire, il faut comprendre que cette matière n’est faite que de logique. Et c’est cette dernière que vous devez apprendre à déchiffrer. Avec un peu de travail sur Fée des maths, vous parviendrez sûrement à réhausser votre moyenne en maths !</div>
             <div class="col-7">
                 <div class="row justify-content-center">
                     <iframe title="video" src="https://www.youtube.com/watch?v=wrNLKRX99Og"></iframe>
                 </div>
-                <p>Taciti justo curabitur augue, sociosqu hendrerit. Bibendum condimentum est a cursus volutpat habitant. Interdum eu nisl sem euismod tempor ipsum aliquet, vel inceptos imperdiet. Tortor eros quisque netus neque sociis erat aptent dictum lorem. A, scelerisque blandit facilisis augue congue primis ante dolor inceptos scelerisque consectetur mauris? Justo orci vestibulum ligula gravida! Fermentum magna facilisi sodales duis vitae a tempor penatibus. Hendrerit penatibus suspendisse consectetur mus fames litora nulla purus. Potenti auctor natoque nostra pretium lorem imperdiet. Integer curae;.</p>
+                <p>Le cours de maths a été compliqué ? Vous ne l’avez pas compris ou peu ? Cela arrive évidemment. Et vous vous dîtes que vous comprendrez mieux au prochain cours ? C’est possible, cependant la meilleure solution pour vous reste de creuser la question vous-même en prenant le temps. Servez-vous de nos cours vidéos et quizz ! Une fois le cours assimilé, jetez-vous dans le bain avec les exercies et prenez le risque de faire des erreurs. Les exercices corrigés vous permettront de les identifier pour mieux les comprendre. Ceci est probablement l’une des meilleures méthodes pour progresser en maths !</p>
             </div>
         </div>
         </div>
